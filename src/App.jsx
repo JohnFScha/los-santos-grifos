83907b790a63df0a422300219d2ff33dd137004e
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-11-08 19:47:25 -0300

    add: agregue react router y estableci las rutas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from './pages/Home'
+import Products from './pages/Products'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/productos" element={<Products/>} />
+        <Route path="/nosotros" element={<About/>} />
+        <Route path="/contacto" element={<Contact/>} />
+      </Routes>
+      <Footer />
+    </main>
+  );
 }
 
-export default App
+export default App;
