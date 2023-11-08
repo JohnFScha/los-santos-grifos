93b2e4855b68bdcf65695d7926c5e7f2741d16c1
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-11-08 20:13:04 -0300

    add: puse el provider de chakra en el main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Router>
   </React.StrictMode>
 );
