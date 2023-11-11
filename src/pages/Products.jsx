98692ca3880936b29e2795046307a04221a9c5cd
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-11-11 11:13:02 -0300

    agrego imagenes de productos con textos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import ImageWithText from "../components/Products/ImageWithText";
+import "../components/Products/ImageWithText.css";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <section id="accesorios">
+        <div>
+          <h2>Nuestros productos</h2>
+          <ImageWithText
+            imageUrl="/img/imgProducts.png"
+            title="Griferías y accesorios"
+            description="Lorem ipsum dolor sit amet consectetur."
+            imageClassName="contenedor-img1"
+            imgProductClassName="imagen-contenedor1"
+            contenedorImageClassName="contenedor1"
+            textImgClassName="text-contenedor-producto1"
+          />
+          <ImageWithText
+            imageUrl="/img/cocina-minimalista1.png"
+            title="Bachas artesanales"
+            description="Lorem ipsum dolor sit amet consectetur."
+            imageClassName="contenedor-img2"
+            imgProductClassName="imagen-contenedor2"
+            contenedorImageClassName="contenedor2"
+            textImgClassName="text-contenedor-producto2"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
