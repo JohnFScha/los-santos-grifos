0d36556a24f9fceca62e4829254cef83925aeb58
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-11-11 17:17:07 -0300

    termino page productos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ImageWithText from "../components/Products/ImageWithText";
+import Boton from "../components/Buttons/Button";
 import "../components/Products/ImageWithText.css";
+import "../components/Buttons/Button.css";
+import "../css/products.css";
 
 const Products = () => {
   return (
     <div>
       <section id="accesorios">
-        <div>
+        <div className="products">
           <h2>Nuestros productos</h2>
           <ImageWithText
             imageUrl="/img/imgProducts.png"
@@ -26,6 +29,25 @@ const Products = () => {
             contenedorImageClassName="contenedor2"
             textImgClassName="text-contenedor-producto2"
           />
+          <Boton />
+          <div className="contenedor-tarjeta-principal">
+            <div className="container-tarjeta">
+              <div className="tarjeta-sobresalida">
+                <div className="contenedor-image">
+                  <img
+                    className="img-tarjeta"
+                    src="/img/mujer-trabajando.png"
+                    alt=""
+                  />
+                </div>
+                <div className="tarjeta-contenido-texto">
+                  <h3>Lorem ipsum dolor sit.</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur. Feugiat</p>
+                  <Boton />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
