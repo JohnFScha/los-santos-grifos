0cec2f44dda87a80bd9deaeaf757e4a5d2b3875b
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-11-15 18:30:05 -0300

    creo card objetivo

--- src/components/CardsObjetivos/CardObjetivo.jsx
+++ src/components/CardsObjetivos/CardObjetivo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./CardObjetivo.css";
 
 const CardObjetivo = ({ urlIconCard, titleCard, textCard }) => {
   return (
     <div>
       <div className="Card">
         <div className="iconCard">
-          <img className="img-tarjeta" src={urlIconCard} alt="" />
+          <img className="img-tarjeta-circle" src={urlIconCard} alt="" />
         </div>
         <div className="textCard">
           <h4>{titleCard}</h4>
