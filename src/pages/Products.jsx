8733bc57b4801af000c6a6924657e813330c95b9
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-12-06 19:56:09 -0300

    terminada productos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,34 +9,39 @@ const Products = () => {
       <section id="accesorios">
         <div className="products">
           <h2>Nuestros productos</h2>
-          <ImageWithText
-            imageUrl="/img/cocina-minimalista2.png"
-            title="Griferias para cocinas"
-            description="Lorem ipsum dolor sit amet consectetur."
-            imageClassName="contenedor-img2"
-            imgProductClassName="imagen-contenedor2"
-            contenedorImageClassName="contenedor2"
-            textImgClassName="text-contenedor-producto2"
-          />
+
           <ImageWithText
             imageUrl="/img/imgProducts.png"
             title="Griferías y accesorios"
-            description="Lorem ipsum dolor sit amet consectetur."
+            description="Fabricada con materiales de calidad que fusionan practicidad y elegancia."
             imageClassName="contenedor-img1"
             imgProductClassName="imagen-contenedor1"
             contenedorImageClassName="contenedor1"
             textImgClassName="text-contenedor-producto1"
           />
           <ImageWithText
-            imageUrl="/img/cocina-minimalista1.png"
-            title="Bachas artesanales"
-            description="Lorem ipsum dolor sit amet consectetur."
+            imageUrl="/img/cocina-minimalista2.png"
+            title="Griferias para cocinas"
+            description="Variedad de modelos importados que aportan durabilidad, comodidad y estética."
             imageClassName="contenedor-img2"
             imgProductClassName="imagen-contenedor2"
             contenedorImageClassName="contenedor2"
             textImgClassName="text-contenedor-producto2"
           />
-          <Button>Descargar Catalogo</Button>
+          <ImageWithText
+            imageUrl="/img/cocina-minimalista1.png"
+            title="Bachas artesanales"
+            description="Piezas únicas y exclusivas realizadas
+            en cerámica de alta calidad."
+            imageClassName="contenedor-img2"
+            imgProductClassName="imagen-contenedor2"
+            contenedorImageClassName="contenedor2"
+            textImgClassName="text-contenedor-producto2"
+          />
+          <div style={{ marginTop: "20px", marginBottom: "60px" }}>
+            <Button>Descargar Catalogo</Button>
+          </div>
+
           <ImageWithContainer
             imageUrlTarjeta="/img/mujer-trabajando.png"
             customClass="imagenMujer"
