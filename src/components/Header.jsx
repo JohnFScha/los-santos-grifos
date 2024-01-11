7535901a18de1578932c36b3b72c0d48cab29c7b
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-01-11 15:48:55 -0300

    fix header logo link to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   const isLinkActive = (pathname) => {
     // Compare the current pathname with the link's pathname
@@ -14,7 +14,9 @@ const Header = () => {
 
   return (
     <header>
-      <img src={Logo} alt="Los santos grifos" />
+      <Link to={'/'}>
+        <img src={Logo} alt="Los santos grifos" />
+      </Link>
       <nav id="mobile-menu">
         <Menu>
           <MenuButton>
@@ -40,11 +42,30 @@ const Header = () => {
         </Menu>
       </nav>
       <nav id="desktop-menu">
-        <Link to={"/"} className={isLinkActive("/") ? "active" : ""}>Inicio</Link>
-        <Link to={"/productos"} className={isLinkActive("/productos") ? "active" : ""}>Nuestros productos</Link>
-        <Link to={"/nosotros"} className={isLinkActive("/nosotros") ? "active" : ""}>Quienes Somos</Link>
-        <Link to={"/faq"} className={isLinkActive("/faq") ? "active" : ""}>Preguntas frecuentes</Link>
-        <Link to={"/contacto"} className={isLinkActive("/contacto") ? "active" : ""}>Contacto</Link>
+        <Link to={"/"} className={isLinkActive("/") ? "active" : ""}>
+          Inicio
+        </Link>
+        <Link
+          to={"/productos"}
+          className={isLinkActive("/productos") ? "active" : ""}
+        >
+          Nuestros productos
+        </Link>
+        <Link
+          to={"/nosotros"}
+          className={isLinkActive("/nosotros") ? "active" : ""}
+        >
+          Quienes Somos
+        </Link>
+        <Link to={"/faq"} className={isLinkActive("/faq") ? "active" : ""}>
+          Preguntas frecuentes
+        </Link>
+        <Link
+          to={"/contacto"}
+          className={isLinkActive("/contacto") ? "active" : ""}
+        >
+          Contacto
+        </Link>
       </nav>
     </header>
   );
