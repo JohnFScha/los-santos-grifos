f7663fc0fed37c827ce26c2c2079040c3d7d68db
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-04-24 17:36:55 -0300

    add: redirect to dropbox catalog

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,20 +1,12 @@
-// import React from "react";
-// import "./Button.css";
-// const Button = ({ children }) => {
-//   return (
-//     <div className="boton">
-//       <button className="botonDescargar">{children}</button>
-//     </div>
-//   );
-// };
 
-// export default Button;
 import React from "react";
 import "./Button.css";
+import { Link } from "react-router-dom";
+
 const Button = ({children}) => {
   return (
     <div className="boton">
-      <button className="botonDescargar">{children}</button>
+      <Link className="botonDescargar" to={'https://www.dropbox.com/scl/fo/j3bwmqiwc4k7kc4egdy8u/AKXNCtX-VkBKTpG5ofRX51o?rlkey=g1fzjkb91uu6z1wt2yco3lkp2&e=1&st=6yrjydnt&dl=0'} target="_blank" rel="noopener noreferrer">{children}</Link>
     </div>
   );
 };
